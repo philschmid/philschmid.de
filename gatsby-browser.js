4b4bc86acb63716eeda1b4df84a3128c69234ec4
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-10-25 10:44:15 +0100

    fixed . in excerpt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,30 @@
 import './src/styles/index.css';
 import 'katex/dist/katex.min.css';
 
-// const ReactGA = require('react-ga');
-// const uuid = require('uuid');
+const ReactGA = require('react-ga');
+const uuid = require('uuid');
 
-// const GA_LOCAL_STORAGE_KEY = 'ga:clientId';
-// // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+const GA_LOCAL_STORAGE_KEY = 'ga:clientId';
+// ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
-// // const state = localStorage.getItem('Privacy');
-// if (localStorage) {
-//   if (localStorage.getItem(GA_LOCAL_STORAGE_KEY)) {
-//     ReactGA.initialize('UA-154999168-1', {
-//       gaOptions: {
-//         storage: 'none',
-//         clientId: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
-//       },
-//     });
-//   } else {
-//     localStorage.setItem(GA_LOCAL_STORAGE_KEY, uuid.v4());
-//     ReactGA.initialize('UA-154999168-1', {
-//       gaOptions: {
-//         storage: 'none',
-//         clientId: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
-//       },
-//     });
-//   }
+// const state = localStorage.getItem('Privacy');
+if (localStorage) {
+  if (localStorage.getItem(GA_LOCAL_STORAGE_KEY)) {
+    ReactGA.initialize('UA-154999168-1', {
+      gaOptions: {
+        storage: 'none',
+        clientId: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
+      },
+    });
+  } else {
+    localStorage.setItem(GA_LOCAL_STORAGE_KEY, uuid.v4());
+    ReactGA.initialize('UA-154999168-1', {
+      gaOptions: {
+        storage: 'none',
+        clientId: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
+      },
+    });
+  }
 
-//   console.log('init');
-// }
+  // console.log('init');
+}
